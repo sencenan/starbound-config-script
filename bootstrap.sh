697d9f6eac7574d29b03ed30462314656c40383d
sencenan <sen.cenan@gmail.com>
2016-10-30 22:57:51 -0400

    startserver

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 rm -rf /home/ubuntu/starbound-config-script
 mkdir -p /home/ubuntu/starbound-config-script
 aws s3 cp --recursive s3://starbound-config/script /home/ubuntu/starbound-config-script/
+chmod +x /home/ubuntu/starbound-config-script/onstart.sh
 /home/ubuntu/starbound-config-script/onstart.sh
