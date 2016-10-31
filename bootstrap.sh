789ecd574b323cf4fb373283db12fcaed5be9518
Chenhan Shen <sencenan@users.noreply.github.com>
2016-10-31 13:20:31 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf /home/ubuntu/starbound-config-script
 mkdir -p /home/ubuntu/starbound-config-script
-aws s3 cp --recursive s3://starbound-config/script /home/ubuntu/starbound-config-script/
+/usr/local/bin/aws s3 cp --recursive s3://starbound-config/script /home/ubuntu/starbound-config-script/
 chmod +x /home/ubuntu/starbound-config-script/onstart.sh
 /home/ubuntu/starbound-config-script/onstart.sh
